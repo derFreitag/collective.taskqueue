049b0c3c4583f80734cc0ace1472700be5e877e3
Gil Forcada <gil.gnome@gmail.com>
2021-04-21 09:58:12 +0200

    refactor: do not use ZConfig
    The configuration depended on ZServer ZConfig, but I am clueless on how
    to strip that apart.
    
    Now configuration will be done via environment variables and only one
    queue can be set up.

--- src/collective/taskqueue/__init__.py
+++ src/collective/taskqueue/__init__.py
@@ -2,3 +2,6 @@
 from collective.taskqueue.taskqueue import LocalVolatileTaskQueue as local
 from collective.taskqueue.redisqueue import RedisTaskQueue as redis
 
+from collective.taskqueue.datatypes import create_taskqueue
+
+create_taskqueue()

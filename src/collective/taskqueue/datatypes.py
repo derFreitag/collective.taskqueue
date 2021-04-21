e2d536d9c0e29a9231f85a7628c41fcb3e00b683
Gil Forcada <gil.gnome@gmail.com>
2021-04-21 09:58:12 +0200

    refactor: do not use ZConfig
    The configuration depended on ZServer ZConfig, but I am clueless on how
    to strip that apart.
    
    Now configuration will be done via environment variables and only one
    queue can be set up.

--- src/collective/taskqueue/datatypes.py
+++ src/collective/taskqueue/datatypes.py
@@ -3,22 +3,24 @@ from collective.taskqueue.config import TASK_QUEUE_IDENT
 from collective.taskqueue.interfaces import ITaskQueue
 from zope.component import provideUtility
 
+import os
+
 
 class TaskQueueFactory(object):
-    def __init__(self, section):
+    def __init__(self, data):
         self.ip = None
         self.port = None
         self.host = None
         self.server_name = TASK_QUEUE_IDENT
 
-        self.queue = section.queue
-        self.type = section.type
+        self.queue = data['queue']
+        self.type = data['type']
         self.kwargs = {
-            "host": section.host,
-            "port": section.port,
-            "db": section.db,
-            "password": section.password,
-            "unix_socket_path": section.unix_socket_path,
+            "host": data['host'],
+            "port": data['port'],
+            "db": data['db'],
+            "password": data['password'],
+            "unix_socket_path": data['unix_socket_path'],
         }
 
         # Drop empty or conflicting kwargs
@@ -55,3 +57,17 @@ class TaskQueueFactory(object):
         task_queue.server_name = "%s:%s" % (self.server_name, self.queue)
 
         return task_queue
+
+
+def create_taskqueue():
+    data = {
+        'queue': os.environ.get('TASKQUEUE_QUEUE', 'default'),
+        'type': os.environ.get('TASKQUEUE_TYPE', 'local'),
+        'host':  os.environ.get('TASKQUEUE_HOST', 'localhost'),
+        'port':  int(os.environ.get('TASKQUEUE_PORT', 6379)),
+        'db':  int(os.environ.get('TASKQUEUE_DB', 0)),
+        'password':  os.environ.get('TASKQUEUE_PASSWORD', ''),
+        'unix_socket_path':  os.environ.get('TASKQUEUE_SOCKET', ''),
+    }
+    taskqueue = TaskQueueFactory(data)
+    taskqueue.create()
